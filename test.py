ef63d512b6404555ecd013896796257ccf1cf64f
siradawhqn@gmail.com <siradawhqn@gmail.com>
2024-01-29 23:42:52 +0700

    uncomment i*2

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 name = input('What is your name?\n')	 
 print(">>>>",name)
 
-# for i in range(1, 13):
-# 	first = i*2
-# 	print(i,"X 2 =",first)
+for i in range(1, 13):
+	first = i*2
+	print(i,"X 2 =",first)
 
 for i in range(1,5):
 	for j in range(i) :
 		print(i,end=" ")
 	print()
+	
+
